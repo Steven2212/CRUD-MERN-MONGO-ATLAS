2ef280c69b5df54f3fa0cdd2ff33d15cdee7ae4a
Steven F <stevenofficial9798@gmail.com>
2022-11-08 21:54:36 +0530

    Search Added with Debounce

--- Frontend/src/components/GetData.js
+++ Frontend/src/components/GetData.js
@@ -6,7 +6,7 @@ import {Pagination} from "antd";
 
 const GetData = (props) => {
 
-  const [userlist, setUserlist] = useState([]);
+  // const [userlist, setUserlist] = useState([]); //We will use this when we won't use Pagination.
 
   //For Pagination
 
@@ -14,11 +14,11 @@ const GetData = (props) => {
   const [total, setTotal] = useState("")
   const [page, setPage] = useState(1)
   const [postPerPage, setPostPerPage] = useState(10) //keep it 10 or 5.
-
-const indexOfLastPage = page * postPerPage
-const indexOfFirstPage = indexOfLastPage - postPerPage
-const currentPosts = posts.slice(indexOfFirstPage, indexOfLastPage)
-
+  
+  const indexOfLastPage = page * postPerPage
+  const indexOfFirstPage = indexOfLastPage - postPerPage
+  const currentPosts = posts.slice(indexOfFirstPage, indexOfLastPage)
+  
 const onShowSizeChange =(current,pageSize)=>{
   setPostPerPage(pageSize)
 }
@@ -43,7 +43,7 @@ const itemRender = (current,type,originalElement) =>{
   const getUserData = async () => {
     let result = await fetch("http://localhost:5000/userdata");
     result = await result.json();
-    setUserlist(result);
+    // setUserlist(result); //Use this when we are not using Pagination.
     setPosts(result);
     setTotal(result.length)
 
@@ -64,19 +64,37 @@ const itemRender = (current,type,originalElement) =>{
 
   //To Search Data :
 
+  //Using debounce in search we can make a search api request in some time (500 milliseconds).
+  const debounce = (func)=>{
+  let timer;
+  return function (...args){
+    const context = this;
+    if(timer) clearTimeout(timer)
+    timer = setTimeout(()=>{
+      timer = null
+      func.apply(context,args);
+    }, 500)
+  }
+}
+
   const searchHandle = async (event) => {
     let key = event.target.value; 
     if (key) {
       let result = await fetch(`http://localhost:5000/search/${key}`);
       result = await result.json();
       if (result) {
-        setUserlist(result); 
-        console.log(setUserlist);
+        // setUserlist(result); //Use this when we are not using Pagination.
+        setPosts(result); //Since we are using Pagination...
+        console.log(setPosts);
       }
     } else {
       getUserData(); //if no words are searched then whole list of data will be shown.
     }
   };
+
+  // const optimisedSearch = useCallback( debounce( searchHandle), [])
+  const optimisedSearch = debounce( searchHandle)
+  
 
   return (
     <>
@@ -86,7 +104,9 @@ const itemRender = (current,type,originalElement) =>{
 
       <input
         className="search-input"
-        onChange={searchHandle}
+        // onChange={searchHandle} //Use this if we are using search without debouncing.
+        onChange={optimisedSearch} //With debouncing.
+
         type="search"
         placeholder="Search"
       />
@@ -107,7 +127,7 @@ const itemRender = (current,type,originalElement) =>{
               <td>Delete</td>
               <td>Update</td>
             </tr>
-            {currentPosts.length > 0 ? (
+            {currentPosts.length > 0 ? ( //use userList.length and userList.map without Pagination.
               currentPosts.map((item, index) => (
                 <tr key={item._id}>
                   <td>{index + 1}</td>
